9fccfef791465e9672d9f45dcf942517fe01a1f1
Bobobibo <124425431+Bobobibo@users.noreply.github.com>
2023-10-24 20:21:42 +0300

    Add files via upload

--- Card.java
+++ Card.java
@@ -1,45 +1,68 @@
-import java.util.Random;
+import java.util.* ;
+// import java.util.Scanner;
 
 
 public class Card {
-         String symbol;
-         String value;
-         //private static final String[] values;
-        // private static final String[] symbols;
-       //  static{
-             String[] values = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q", "A"};
-             String[] symbols = {"♥", "♦", "♣", "♠"};
+    String symbol;
+    String value;
+    //private static final String[] values;
+    // private static final String[] symbols;
+    //  static{
+    String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q", "A"};
+    String[] symbols = {"♥", "♦", "♣", "♠"};
 
-        // }
-             Random rand = new Random();
+    // }
+    Random rand = new Random();
 
-             private class card{
-                 String value;
-                 String symbol;
+    private class card {
+        String value;
+        String symbol;
 
 
     }
-         public card getCards() {
 
-             card card = new card();
-             int card1 = rand.nextInt(14);
-             int mast = rand.nextInt(4);
-             card.value = values[card1 - 1];
-             card.symbol = symbols[mast - 1];
-            // System.out.println(this.value+this.symbol);
+    public card getCards() {
 
-             return card;
+        card card = new card();
+        int card1 = rand.nextInt(14);
+        int mast = rand.nextInt(4);
+        card.value = values[card1];
+        card.symbol = symbols[mast];
+        // System.out.println(this.value+this.symbol);
 
-         }
+        return card;
 
-         private Integer getValue(card card){
-                 if (card.value== "K" || card.value=="J"  ||card.value== "Q"){
-                        return 10;
-                 }else if (card.value=="A"){
-                     return 11;
-                 }
-                 int value = card.value
-                 return card.value
+    }
 
-         }
-}
+    public Integer getValue(card card) {
+        if (card.value.equals("K") || card.value.equals("J") || card.value.equals("Q")) {
+            return 10;
+        } else if (card.value.equals("A")) {
+            return 11;
+        } else {
+
+            return (Integer.parseInt(card.value) + 2);
+
+        }
+
+    }
+
+
+    public class Player {
+
+        private class name{
+            Scanner namescanner = new Scanner(System.in);
+            Integer name = namescanner.nextInt();
+        }
+        int defaultbalance = 500;
+
+        Card[] card() {
+            return new Card[0];
+        }
+        public class points{
+        Card.getValue();
+        }
+
+
+    }
+
---
+++ Player.java
@@ -0,0 +1,20 @@
+import java.util.* ;
+
+
+public class Player {
+
+    private class name{
+        Scanner namescanner = new Scanner(System.in);
+        Integer name = namescanner.nextInt();
+    }
+    int defaultbalance = 500;
+
+    Card[] card() {
+        return new Card[0];
+    }
+    public class points{
+        Card.getValue();
+    }
+
+
+}
