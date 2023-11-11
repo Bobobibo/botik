d1bbb3b508fdf2a5e70871a03752947ab8091fbb
Bobobibo <124425431+Bobobibo@users.noreply.github.com>
2023-11-11 21:14:41 +0200

    Add files via upload

--- Blackjack.java
+++ Blackjack.java
@@ -9,106 +9,127 @@ public class Blackjack {
     static Player playername = new Player();
     static Turn playercardd = new Turn();
     static Turn dilercardd = new Turn();
+
+    static Turn deck= new Turn();
+    static Turn playerdeck=new Turn();
     static Boolean situation = false;
 
     public static void main(String[] args) {
 
         int dilerhand = 0;
         int playerhand = 0;
-        String pname = playername.getName();
+        String pname = Player.getName();
         Scanner decide = new Scanner(System.in);
-        System.out.println("Your turn?: ");
+        System.out.println("Your turn?(start or stop): ");
         String decision = decide.next();
         switch (decision) {
             case "start":
                 System.out.println(pname + "'s turn: ");
-                Integer x1 = playercardd.getCard();
-                Integer x2 = playercardd.getCard();
+                Integer x1 = Turn.getCard();
+                Integer x2 = Turn.getCard();
                 playerhand = x1 + x2;
+                playerdeck.playerShowdeck();
                 System.out.println("Diler's turn: ");
-                Integer x11 = dilercardd.getCard();
-                Integer x21 = dilercardd.getCard();
+                Integer x11 = Turn.dilerfirstTurn();
+                Integer x21 = Turn.dilerTurn();
                 dilerhand = x11 + x21;
-            case "stop":
 
-                break;
-        }
-        for (; ; ) {
-            if (playerhand > 21) {
-                System.out.println("you lost");
+            case "stop":
+                System.out.println("No game");
                 situation = true;
                 break;
 
-            }
-            System.out.println("Your turn?: ");
-            Scanner decide2 = new Scanner(System.in);
-            String decision2 = decide2.next();
-            switch (decision2) {
-                case "take":
-                    System.out.println(pname + "'s turn: ");
-                    //Integer x23 = playercardd.getCard();
-                    playerhand += playercardd.getCard();
+        }
+        if(!decision.equals("stop")){
+            for (;;) {
+
                     if (playerhand > 21) {
                         System.out.println("you lost");
                         situation = true;
                         break;
 
                     }
+           // if (situation.equals(true)) {
+               // break;
 
+           // }
 
-                    System.out.println("Diler's turn: ");
-                    //Integer x111 = dilercardd.getCard();
-                    dilerhand += dilercardd.getCard();
-                    if (dilerhand > 21) {
-                        System.out.println("diler lost");
-                        situation = true;
-                        break;
+                    for(;;){
 
+                        System.out.println("Your turn?(take or hold): ");
+                        Scanner decide2 = new Scanner(System.in);
+                        String decision2 = decide2.next();
+                        switch (decision2) {
+                            case "take":
+                                System.out.println(pname + "'s turn: ");
+                                playerhand += Turn.getCard();
+                                playerdeck.playerShowdeck();
+                                if (playerhand > 21) {
+                                    System.out.println("you lost");
+                                    situation = true;
+                                    break;
+
+                                 }
+                            case "hold":
+                                for (; ; ) {
+                                    if (dilerhand < 17) {
+                                        dilerhand += Turn.dilerTurn();
+                                    }
+                                     if (dilerhand > 21) {
+                                        playerdeck.playerShowdeck();
+                                        deck.dillerShowdeck();
+                                        System.out.println("diler lost");
+                                        situation = true;
+                                        break;
+                                    }  if (dilerhand == 21 && playerhand < 21) {
+                                        playerdeck.playerShowdeck();
+                                        deck.dillerShowdeck();
+                                        System.out.println("diler won");
+                                        situation = true;
+                                        break;
+                                    } if (dilerhand == 21 && playerhand == 21) {
+                                        playerdeck.playerShowdeck();
+                                        deck.dillerShowdeck();
+                                        System.out.println("draw");
+                                        situation = true;
+                                        break;
+                                    } if (dilerhand == 21 && playerhand > 21) {
+                                        playerdeck.playerShowdeck();
+                                        deck.dillerShowdeck();
+                                        System.out.println("diler won");
+                                        situation = true;
+                                        break;
+                                    }  if (dilerhand < 21 && dilerhand >= 17 && dilerhand < playerhand) {
+                                        playerdeck.playerShowdeck();
+                                        deck.dillerShowdeck();
+                                        System.out.println("diler lost");
+                                        situation = true;
+                                        break;
+                                    } if (dilerhand < 21 && dilerhand >= 17 && dilerhand > playerhand) {
+                                        playerdeck.playerShowdeck();
+                                        deck.dillerShowdeck();
+                                        System.out.println("you lost");
+                                        situation = true;
+                                        break;
+                                    }
+                                    if (situation.equals(true)) {
+                                        break;
+                                    }
+                                }
+                            if (situation.equals(true)) {
+                                    break;
+                            }
+                        }
+                        if (situation.equals(true)) {
+                            break;
+                        }
                     }
-                case "hold":
-                    if (dilerhand < 17) {
-                        // dilercardd.getCard();
-                        dilerhand += dilercardd.getCard();
-                    }
-                    if (dilerhand > 21) {
-                        System.out.println("diler lost");
-                        situation = true;
-                        break;
-                    }
-                    if (dilerhand == 21 && playerhand < 21) {
-                        System.out.println("diler won");
-                        situation = true;
-                        break;
-                    } else if (dilerhand == 21 && playerhand == 21) {
-                        System.out.println("draw");
-                        situation = true;
-                        break;
-                    } else if (dilerhand == 21 && playerhand > 21) {
-                        System.out.println("diler won");
-                        situation = true;
-                        break;
-
-
-                    } else if (dilerhand < 21 && dilerhand >= 17 && dilerhand < playerhand) {
-                        System.out.println("diler lost");
-                        situation = true;
-                        break;
-
-                    } else if (dilerhand < 21 && dilerhand >= 17 && dilerhand > playerhand) {
-                        System.out.println("you lost");
-                        situation = true;
-                        break;
-                    }
+                if (situation.equals(true)) {
+                    break;
+                }
             }
-
-        if (situation.equals(true)) {
-                        break;
-
-          }
-
-
-
-
         }
+
     }
+
 }
--- Turn.java
+++ Turn.java
@@ -1,29 +1,61 @@
-import java.io.*;
+import java.util.*;
 
 public class Turn {
 
+
+    static ArrayList<String> playerdeck = new ArrayList<String>();
+    static ArrayList<String> dilerdeck = new ArrayList<String>();
     static Card playercard = new Card();
     static Card dilercard = new Card();
+
+
 
     static Integer getCard() {
         String y = playercard.getCards();
         Integer x = playercard.getValue(); // get value in int
-
         System.out.println(y);
-        System.out.println(x);
-
-
+        System.out.println("Card value: " + x);
+        playerdeck.add(y);
+        //System.out.println(y);
+       // System.out.println(x);
+       // System.out.println("Your deck:" + playerdeck);
         return x;
 
     }
-    static Integer dilerTurn(){
+    static Integer dilerfirstTurn(){
         String y1 = dilercard.getCards();
         Integer x1 = dilercard.getValue(); // get value in int
 
         System.out.println(y1);
-        System.out.println(x1);
+        System.out.println("Card value: " + x1);
+        dilerdeck.add(y1);
         return x1;
 
     }
+
+    static Integer dilerTurn(){
+        String y1 = dilercard.getCards();
+        Integer x1 = dilercard.getValue(); // get value in int
+        System.out.println("?");
+        System.out.println("Card value: ?");
+        dilerdeck.add(y1);
+
+        return (x1);
+
+
+    }
+    String dillerShowdeck(){
+    System.out.println("Diller's deck: " + dilerdeck);
+
+        return null;
+    }
+    String playerShowdeck(){
+
+    System.out.println("Player's deck: " + playerdeck);
+
+            return null;
+    }
+
+
 
 }
